6efe622493619272f7e9e06a77bfff904f9cb65b
Jerry Xiong <jxiong60@gatech.edu>
2022-09-30 15:01:42 -0400

    Write partially working draft of log binary search

--- tuning.py
+++ tuning.py
@@ -1,6 +1,7 @@
 import itertools
 import math
-from collections import deque
+import random
+from dataclasses import dataclass
 from fractions import Fraction
 
 from ray.tune.search import Searcher
@@ -12,6 +13,14 @@ def log_binary_search(*values):
 
 def grid_search(*values):
     return {"grid_search": values}
+
+
+@dataclass
+class SearchNode:
+    config: dict
+    parent_config: dict
+    depth: int
+    expanded: bool = False
 
 
 class LogBinarySearch(Searcher):
@@ -79,12 +88,16 @@ class LogBinarySearch(Searcher):
                 else:
                     raise ValueError(f"unexpected search space: {space_type}")
 
-        self.candidates = deque()
-        for cfg in itertools.product(start_values):
-            self.candidates.append({k: v for k, v in zip(keys, cfg)})
+        self.candidates = []
+        for spec in itertools.product(*start_values):
+            self.candidates.append(
+                SearchNode({k: v for k, v in zip(keys, spec)}, None, 0)
+            )
 
-        self.best_configs = [None for _ in range(self.max_depth)]
-        self.seen = set()
+        self.best_configs = {}
+        self.best_scores = {}
+        self.in_progress = {}
+        self.completed = set()
 
     def _neighbors_of(self, config, new_depth):
         keys = []
@@ -98,33 +111,80 @@ class LogBinarySearch(Searcher):
             keys.append(k)
             values.append(
                 [
-                    10 ** (config[k] + c * self.log_bin_scales[k] / (2**new_depth))
+                    10.0 ** (Fraction(math.log10(config[k])).limit_denominator(2 * (2 ** self.max_depth)) + c * self.log_bin_scales[k] / (2**new_depth))
                     for c in range(-self.log_bin_sizes[k], self.log_bin_sizes[k] + 1)
                 ]
             )
             assert len(values) == self.log_bin_sizes[k] * 2 + 1  # TODO remove
 
-        for cfg in itertools.product(values):
+        for cfg in itertools.product(*values):
             yield {k: v for k, v in zip(keys, cfg)}
 
     def suggest(self, trial_id):
-        # TODO: rewrite with SearchNode, generate new nodes from candidates
-        # TODO: use a refcount dictionary, which serves secondary purpose as seen
-        # TODO: config hash is just hash of values (with a consistent ordering)
-        # TODO: make sure that children recurse failures to their children
-        # TODO: mitigate bias by randomly shuffling within each depth
-        if len(self.candidates) == 0:
-            for d in range(self.max_depth - 1):
-                found = False
-                for neighbor_cfg in self._neighbors_of(self.best_configs[d], d + 1):
-                    if neighbor_cfg not in self.seen:
-                        self.seen.add(neighbor_cfg)
-                        self.candidates.append(neighbor_cfg)
-                        found = True
-                if found:
-                    break
+        self.candidates = [
+            node
+            for node in self.candidates
+            if (
+                node.depth - 1 not in self.best_configs
+                or node.parent_config == self.best_configs[node.depth - 1]
+            )
+            and not any(nd.config == node.config for nd in self.in_progress.values())
+        ]
 
-        return self.candidates.popleft()
+        if len(self.candidates) == 0:
+            possible_parents = [node for node in self.in_progress.values() if not node.expanded and node.depth < self.max_depth]
+            if len(possible_parents) == 0:
+                return Searcher.FINISHED
+            minimum_depth = min(node.depth for node in possible_parents)
+            parent = random.choice(
+                [node for node in possible_parents if node.depth == minimum_depth]
+            )
+
+            print(f">> expanding {parent} <<")
+
+            parent.expanded = True
+            for config in self._neighbors_of(parent.config, parent.depth + 1):
+                self.candidates.append(
+                    SearchNode(config, parent.config, parent.depth + 1)
+                )
+
+            return self.suggest(trial_id)
+
+        valid = [node for node in self.candidates if node.depth == self.candidates[0].depth]
+        selected = random.choice(valid)
+        self.candidates.remove(selected)
+        self.in_progress[trial_id] = selected
+        print(f"<< suggesting {selected} >>")
+        return selected.config
 
     def on_trial_complete(self, trial_id, result=None, error=False):
-        pass
+        node = self.in_progress[trial_id]
+        if (
+            node.depth not in self.best_configs
+            or (
+                self.mode == "max"
+                and result[self.metric] > self.best_scores[node.depth]
+            )
+            or (
+                self.mode == "min"
+                and result[self.metric] < self.best_scores[node.depth]
+            )
+        ):
+            self.best_scores[node.depth] = result[self.metric]
+            self.best_configs[node.depth] = node.config
+
+        del self.in_progress[trial_id]
+
+
+searcher = LogBinarySearch(
+    {
+        "lr": log_binary_search(1e-3, 1e-2, 1e-1),
+        "batch_size": grid_search(32, 64),
+        "n_layers": 2,
+    },
+    depth=2,
+    metric="val_acc",
+    mode="max",
+)
+for i in range(100):
+    searcher.suggest(str(i))
